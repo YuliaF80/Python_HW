980fdaf9effb029c410dd82efcbfc07d7ef0dd45
Dmitry Chitalov <35995629+dmitrychitalov@users.noreply.github.com>
2022-03-23 17:16:42 +0500

    Ответы на задания Урока 1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,7 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+n = int(input('Введите число n: '))
+sum_n = n + n * 11 + n * 111
+print(f'n + nn + nnn = {sum_n}')
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,12 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+
+number = int(input('Введите целое положительное число: '))
+max_n = 0
+while number != 0:
+    leftover_number = number % 10
+    if max_n < leftover_number:
+        max_n = leftover_number
+    number = number // 10
+print(f'Самоя большая цифра в числе: {max_n}')
